04e09b18eb66abbcd393de16301456a7f13218dc
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2025-05-20 12:42:34 -0300

    add repo basic infra / folder structure

---
+++ Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using FCG.Domain.Entities;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+
+namespace FCG.Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        Task CreateAsync(T entity);
+        Task DeleteAsync(ObjectId id);
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> filter);
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(ObjectId id);
+        Task UpdateAsync(ObjectId id, T entity);
+    }
+}
---
+++ Infraestructure/Repositories/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using FCG.Domain.Entities;
+using FCG.Domain.Interfaces.Repositories;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace FCG.Infraestructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly IMongoCollection<T> _collection;
+
+        public BaseRepository(IMongoDatabase database, string collectionName)
+        {
+            _collection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task<List<T>> GetAllAsync() => await _collection.Find(_ => true).ToListAsync();
+
+        public async Task<T?> GetByIdAsync(ObjectId id)
+        {
+            var filter = Builders<T>.Filter.Eq("_id", id);
+            return await _collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> FindAsync(Expression<Func<T, bool>> filter)
+            => await _collection.Find(filter).ToListAsync();
+
+        public async Task CreateAsync(T entity) => await _collection.InsertOneAsync(entity);
+
+        public async Task UpdateAsync(ObjectId id, T entity)
+        {
+            var filter = Builders<T>.Filter.Eq("_id", id);
+            await _collection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task DeleteAsync(ObjectId id)
+        {
+            var filter = Builders<T>.Filter.Eq("_id", id);
+            await _collection.DeleteOneAsync(filter);
+        }
+    }
+}
