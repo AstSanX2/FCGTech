6939a02cbfd4146bbab7ea5a857111ab3ec18090
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2025-05-20 14:44:40 -0300

    fix folder structure

---
+++ FCG.API/Domain/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using FCG.Domain.Entities;
+using MongoDB.Bson;
+
+namespace FCG.Domain.Interfaces.Services
+{
+
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(ObjectId id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(ObjectId id, T entity);
+        Task DeleteAsync(ObjectId id);
+    }
+
+}
