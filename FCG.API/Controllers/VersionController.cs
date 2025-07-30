0c67f924de5b0ac97d70d41fff8f65dbc0bb43db
Daniel Santos De Sousa <daniel.s.6140@gmail.com>
2025-07-30 16:22:08 -0300

    Add atributo HttpGet e AllowAnonymous  na version control

--- FCG.API/Controllers/VersionController.cs
+++ FCG.API/Controllers/VersionController.cs
@@ -5,11 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FCG.API.Controllers
 {
+
     [Route("[controller]")]
+    [AllowAnonymous]
     [ApiController]
     public class VersionController : ControllerBase
     {
         [AllowAnonymous]
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok("29-05-2025");
